6ff62f7d93d6149a090bcc0a7dc9aa08d8331ea2
ShiphonJhon <siphon@ares.eonet.ne.jp>
2023-10-29 10:25:48 +0900

    WebTimer 原型

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+import { useState } from "react";
+
+var timeCount = 20;
+
+const TimerCompo = () => {
+
+  const [inputText, setInputText ] = useState(" ")
+  const [dispTime, setDispTime] = useState("");
+  const [dispTimeUp, setDispTimeUP] = useState("");
+
+  const parseTime2Count = (strSec) => {
+    var count = parseInt(strSec);
+    return count;
+  }
+
+  const parseCount2Time = (intCount) => {
+    var strSec = intCount.toString();
+    return strSec;
+  }
+
+  //入力 
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    setInputText(event.target.value);
+  }
+
+  //時間設定
+  const handleClick = () => {
+    console.log("Setting");
+    setDispTime(inputText);
+    timeCount = parseTime2Count(inputText);
+    setDispTimeUP(" ");
+  }
+
+  const handleTimer = () => {
+    timeCount = timeCount - 1;
+    console.log(timeCount);
+    setDispTime(parseCount2Time(timeCount));
+    const timerID = setTimeout(handleTimer, 1000);
+
+    if(timeCount <= 0){
+      clearTimeout(timerID);
+      setDispTimeUP("Time UP!!")
+    }
+  }
+
+  const handleStartClick = () => {
+    console.log("Start");
+    handleTimer();
+  }
+
+  const handleResetClick = () => {
+    console.log("Reset");
+    setDispTime(inputText);
+    timeCount = parseTime2Count(inputText);
+    setDispTimeUP(" ");
+  }
+
+  return (
+    <>
+      <h2>Web Timer</h2>
+      <input type="text" onChange={handleChange} ></input>秒
+      <button onClick={handleClick} >設定</button>
+      <p>残り時間：<span>{dispTime}</span>秒</p>
+      <button onClick={ handleStartClick  } >Start</button>
+      <button onClick={ handleResetClick } >Reset</button>
+      <h2>{dispTimeUp}</h2>
+    </>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TimerCompo />
+    </>
   );
 }
 
